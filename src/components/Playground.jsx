9b0f1ab1e26692f2cf8a202e8841317b75d9dff5
Vivekanand P V <vivekanandpv@outlook.com>
2021-11-17 12:53:10 +0530

    useEffect in Playground

--- src/components/Playground.jsx
+++ src/components/Playground.jsx
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { httpClient } from '../http/http-client';
 
 const Playground = (props) => {
+  useEffect(() => {
+    console.log('Playground first render');
+  }, []);
+
   return (
     <>
       <h3>Axios Playground</h3>
