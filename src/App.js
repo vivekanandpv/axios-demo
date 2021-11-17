d449e4aaef85800cbb2fde82880d224833dcd101
Vivekanand P V <vivekanandpv@outlook.com>
2021-11-17 12:42:22 +0530

    composed playground in app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Playground from './components/Playground';
 
 const App = () => {
   return (
     <>
-      <h3>Axios Demo</h3>
+      <div className='container p-5'>
+        <h3>Axios Demo</h3>
+        <hr />
+        <Playground />
+      </div>
     </>
   );
 };
