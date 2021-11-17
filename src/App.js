0717a7553b46ed8ca7ca4273f5ca466aa2fd3a3e
Vivekanand P V <vivekanandpv@outlook.com>
2021-11-17 13:20:18 +0530

    exercise-solution

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Playground from './components/Playground';
+import TodoList from './components/TodoList';
 
 const App = () => {
   return (
@@ -7,7 +8,8 @@ const App = () => {
       <div className='container p-5'>
         <h3>Axios Demo</h3>
         <hr />
-        <Playground />
+        {/* <Playground /> */}
+        <TodoList />
       </div>
     </>
   );
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import { httpClient } from '../http/http-client';
+
+const TodoList = (props) => {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    httpClient
+      .get('todos')
+      .then((response) => setTodos(response.data))
+      .catch((error) => console.log('Error occurred', error.message))
+      .finally(() => console.log('Server communication is completed'));
+  }, []);
+
+  return (
+    <>
+      <h3>Todo List</h3>
+      <table className='table table-striped table-sm table-bordered'>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Title</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {todos.map((t) => (
+            <tr key={t.id}>
+              <td>{t.id}</td>
+              <td>{t.title}</td>
+              <td>
+                {t.completed ? (
+                  <span className='badge rounded-pill bg-success'>
+                    Completed
+                  </span>
+                ) : (
+                  <span className='badge rounded-pill bg-warning'>Pending</span>
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default TodoList;
